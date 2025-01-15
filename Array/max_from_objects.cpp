257e590c567ff08d3ef60a0bef30d258eef4816a
Tanvir Rahman <tanvirrahmanaz@gmail.com>
2025-01-16 02:03:55 +0600

    add all file

---
+++ Array/max_from_objects.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Student
+{
+    public:
+        string name;
+        int roll;
+        int marks;
+};
+int main(){
+    Student a[3];
+
+    for(int i=0;i<3;i++){
+        getline(cin,a[i].name);
+        cin>>a[i].roll>>a[i].marks;
+        cin.ignore();
+    }
+    Student mx;
+    mx.marks = INT_MIN;
+    for(int i=0;i<3;i++){
+        if(a[i].marks>mx.marks){
+            mx = a[i];
+        }
+    }
+    Student min;
+    min.marks = INT_MAX;
+    for(int i=0;i<3;i++){
+        if(a[i].marks<min.marks){
+            min = a[i];
+        }
+    }
+
+    cout<<mx.name<<" "<<mx.roll<<" "<<mx.marks<<endl;
+    cout<<min.name<<" "<<min.roll<<" "<<min.marks<<endl;
+
+    
+}
