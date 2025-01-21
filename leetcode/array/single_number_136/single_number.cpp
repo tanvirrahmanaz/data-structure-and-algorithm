be214142eeb190251f7f89325297be50fd7da7dc
Tanvir Rahman <tanvirrahmanaz@gmail.com>
2025-01-22 01:09:19 +0600

    single number prlbm add

---
+++ leetcode/array/single_number_136/single_number.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[n],frq=0;
+    for(int i=0; i<n;i++){
+        cin>>arr[i];
+    }
+    for(int i=0;i<n;i++){
+        frq = frq ^ arr[i];
+    }
+    cout<<frq;
+    return 0;
+}
+
+
+// leet code solution 
